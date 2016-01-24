3c59814425ef97224fdbd2fbe931f40a3e5fdf18
Alex Wilkerson <alex@uncorrected.com>
2016-01-24 17:34:48 -0600

    finished section 5. need to go through and reread everything so i understand the weird structure going on here.

--- src/rltut/World.java
+++ src/rltut/World.java
@@ -1,6 +1,8 @@
 package rltut;
 
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
 
 public class World {
     private Tile[][] tiles;
@@ -10,10 +12,21 @@ public class World {
     private int height;
     public int height() { return height; }
 
+    private List<Creature> creatures;
+
     public World(Tile[][] tiles){
         this.tiles = tiles;
         this.width = tiles.length;
         this.height = tiles[0].length;
+        this.creatures = new ArrayList<Creature>();
+    }
+
+    public Creature creature(int x, int y){
+        for(Creature c : creatures){
+            if (c.x == x && c.y == y)
+                return c;
+        }
+        return null;
     }
 
     public Tile tile(int x, int y){
@@ -36,6 +49,10 @@ public class World {
             tiles[x][y] = Tile.FLOOR;
     }
 
+    public void remove(Creature other){
+        creatures.remove(other);
+    }
+
     public void addAtEmptyLocation(Creature creature){
         int x;
         int y;
@@ -44,9 +61,17 @@ public class World {
             x = (int)(Math.random() * width);
             y = (int)(Math.random() * height);
         }
-        while (!tile(x,y).isGround());
+        while (!tile(x,y).isGround() || creature(x,y) != null);
 
         creature.x = x;
         creature.y = y;
+        creatures.add(creature);
+    }
+
+    public void update(){
+        List<Creature> toUpdate = new ArrayList<Creature>(creatures);
+        for (Creature creature : toUpdate){
+            creature.update();
+        }
     }
 }
--- src/rltut/screens/PlayScreen.java
+++ src/rltut/screens/PlayScreen.java
@@ -20,10 +20,18 @@ public class PlayScreen implements Screen{
         createWorld();
 
         CreatureFactory creatureFactory = new CreatureFactory(world);
-        player = creatureFactory.newPlayer();
+        createCreatures(creatureFactory);
     }
 
-    public void createWorld(){
+    private void createCreatures(CreatureFactory creatureFactory){
+        player = creatureFactory.newPlayer();
+        
+        for (int i = 0; i < 8; i++){
+            creatureFactory.newFungus();
+        }
+    }
+
+    private void createWorld(){
         world = new WorldBuilder(90, 31)
             .makeCaves()
             .build();
@@ -43,7 +51,11 @@ public class PlayScreen implements Screen{
                 int wx = x + left;
                 int wy = y + top;
 
-                terminal.write(world.glyph(wx, wy), x, y, world.color(wx, wy));
+                Creature creature = world.creature(wx, wy);
+                if (creature != null)
+                    terminal.write(creature.glyph(), creature.x - left, creature.y - top, creature.color());
+                else
+                    terminal.write(world.glyph(wx, wy), x, y, world.color(wx, wy));
             }
         } 
     }
@@ -53,8 +65,6 @@ public class PlayScreen implements Screen{
         int top = getScrollY();
 
         displayTiles(terminal, left, top);
-
-        terminal.write(player.glyph(), player.x - left, player.y - top, player.color());
 
         terminal.writeCenter("-- press [escape] to lose or [enter] to win --", 22);
     }
@@ -77,7 +87,7 @@ public class PlayScreen implements Screen{
             case KeyEvent.VK_B: player.moveBy(-1, 1); break;
             case KeyEvent.VK_N: player.moveBy( 1, 1); break;
         }
-
+        world.update();
         return this;
     }
 }
